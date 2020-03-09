e63c33f415eb50d56e5e757c07d952cf344bed62
Maha-amaze <61777196+Maha-amaze@users.noreply.github.com>
2020-03-09 11:48:01 +0530

    Add files via upload

---
+++ apb_sequence.sv
@@ -0,0 +1,25 @@
+
+//Sequence class
+//----------------
+class apb_model_sequence extends uvm_sequence#(apb_seq_item);
+  `uvm_object_utils(apb_model_sequence)
+  
+  //Constructor
+  //--------
+  function new(string name="apb_model_sequence");
+    super.new(name);
+  endfunction
+  
+  //Body task
+  //---------
+  task body();
+    apb_seq_item txn=new();
+    repeat(50) begin
+    start_item(txn);
+      txn.randomize();
+      finish_item(txn);
+    end
+    
+  endtask
+endclass
+//Sequence class ends here
