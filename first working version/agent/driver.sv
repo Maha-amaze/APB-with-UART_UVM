ac86ed885e920a61a514eeb51041872b06c1582f
Maha-amaze <61777196+Maha-amaze@users.noreply.github.com>
2020-03-11 10:16:00 +0530

    Update driver.sv

--- first working version/agent/driver.sv
+++ first working version/agent/driver.sv
@@ -1,6 +1,7 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
+
 class apb_driver extends uvm_driver #(apb_seq_item);
 
    virtual apb_if vif;
@@ -28,6 +29,7 @@ class apb_driver extends uvm_driver #(apb_seq_item);
     task run_phase(uvm_phase phase);
     forever begin
       seq_item_port.get_next_item(apb_transactor);
+@(posedge vif.PCLK);
       drive();
       seq_item_port.item_done();
     end
@@ -37,26 +39,57 @@ class apb_driver extends uvm_driver #(apb_seq_item);
   // drive - transaction level to signal level
   // drives the value's from seq_item to interface signals
   //---------------------------------------
-    task drive();
-    vif.PSEL  <= 1;
-    vif.PWRITE <= 0;
+     task drive();
+    /*vif.PSEL  <= 1;
+    vif.PENABLE<=apb_transactor.PENABLE;
+    vif.PWRITE <= apb_transactor.PWRITE;
     //@(posedge vif.DRIVER.clk);
     
     vif.PADDR <= apb_transactor.PADDR;
     
-       if(vif.PWRITE==1) begin // write operation
+    if(vif.PWRITE==1)begin 
+   // write operation 
       vif.PWRITE <= apb_transactor.PWRITE;
       vif.PWDATA <= apb_transactor.PWDATA;
+      vif.uart_wr <= 0;
       //@(posedge vif.DRIVER.clk);
-    end
-    else begin //read operation
-      vif.PRDATA=apb_transactor.PRDATA;
+   end
+    else if(vif.PWRITE==0) begin
+  //read operation
+        vif.PRDATA <= apb_transactor.PRDATA;
        //@(posedge vif.DRIVER.clk);
-      apb_transactor.PRDATA = vif.PRDATA;
-    end*/
-    
+vif.PADDR <= apb_transactor.PADDR;
+      //apb_transactor.PRDATA <= vif.PRDATA;
+     end*/
+         
   endtask : drive
 endclass : apb_driver
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
